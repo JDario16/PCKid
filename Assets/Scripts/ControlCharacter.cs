8d2f4cf71357ba276349b3416f66312cf79833cc
JDario16 <jamonto5@eafit.edu.co>
2017-10-09 14:53:55 -0500

    Primera subida de archivos
    Trabajo realizado hasta la fecha 9 octubre de 2017

---
+++ Assets/Scripts/ControlCharacter.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlCharacter : MonoBehaviour {
+	//Declaraciones
+	public float velocidad;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Arriba
+		GetComponent<Rigidbody2D> ().velocity = Vector2.zero;
+
+		if (Input.GetKey(KeyCode.W)) {
+			//transform.Translate (new Vector2 (GetComponent<Rigidbody2D>	().velocity.x, velocidad));
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (GetComponent<Rigidbody2D>	().velocity.x, velocidad);
+		} 
+
+		//Abajo
+		if(Input.GetKey(KeyCode.S)){
+			//transform.Translate (new Vector2 (GetComponent<Rigidbody2D>	().velocity.x, -velocidad));
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (GetComponent<Rigidbody2D>	().velocity.x, -velocidad);
+		} 
+		//Derecha
+		if(Input.GetKey(KeyCode.D)){
+			//transform.Translate (new Vector2 (velocidad, GetComponent<Rigidbody2D>	().velocity.y));
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (velocidad, GetComponent<Rigidbody2D>	().velocity.y);
+		} 
+		//Izquierda
+		if(Input.GetKey(KeyCode.A)){
+			//transform.Translate (new Vector2 (-velocidad, GetComponent<Rigidbody2D>	().velocity.y));
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (-velocidad, GetComponent<Rigidbody2D>	().velocity.y);
+		} 
+	}
+}
