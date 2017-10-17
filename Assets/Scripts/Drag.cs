d56f2e716ed325da7ecfb1ed1c9c9e3c382655fe
JDario16 <jamonto5@eafit.edu.co>
2017-10-17 02:12:20 -0500

    Drag and Drop
    Avance en el drag and drop, implementando el boton de inicio.

---
+++ Assets/Scripts/Drag.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine;
+
+public class Drag : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+
+    public void OnBeginDrag(PointerEventData eventData) {
+        Debug.Log("OnBeginDrag");
+
+        this.transform.position = eventData.position;
+    }
+
+    public void OnDrag(PointerEventData eventData) {
+        Debug.Log("OnDrag");
+
+        this.transform.position = eventData.position;
+        
+    }
+
+    public void OnEndDrag(PointerEventData eventData) {
+        Debug.Log("OnEndDrag");
+
+        this.transform.position = eventData.position;
+    }
+
+    void Update() {
+        Vector3 pos = transform.position;
+        pos.z = 461;
+        transform.position = pos;
+    }
+}
---
+++ Assets/Scripts/DropMe.cs
@@ -0,0 +1,85 @@
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class DropMe : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler
+{
+    public string test;
+	public Image containerImage;
+	public Image receivingImage;
+	private Color normalColor;
+	public Color highlightColor = Color.yellow;
+	
+	public void OnEnable ()
+	{
+		if (containerImage != null)
+			normalColor = containerImage.color;
+	}
+	
+	public void OnDrop(PointerEventData data)
+	{
+		containerImage.color = normalColor;
+		
+		if (receivingImage == null)
+			return;
+
+        string instru = GetDropString(data);
+		Sprite dropSprite = GetDropSprite (data);
+		if (dropSprite != null)
+			receivingImage.overrideSprite = dropSprite;
+        test = instru;
+	}
+
+	public void OnPointerEnter(PointerEventData data)
+	{
+		if (containerImage == null)
+			return;
+		
+		Sprite dropSprite = GetDropSprite (data);
+		if (dropSprite != null)
+			containerImage.color = highlightColor;
+	}
+
+	public void OnPointerExit(PointerEventData data)
+	{
+		if (containerImage == null)
+			return;
+		
+		containerImage.color = normalColor;
+	}
+	
+	private Sprite GetDropSprite(PointerEventData data)
+	{
+		var originalObj = data.pointerDrag;
+		if (originalObj == null)
+			return null;
+		
+		var dragMe = originalObj.GetComponent<DragMe>();
+		if (dragMe == null)
+			return null;
+		
+		var srcImage = originalObj.GetComponent<Image>();
+		if (srcImage == null)
+			return null;
+		
+		return srcImage.sprite;
+	}
+
+    private string GetDropString(PointerEventData data)
+    {
+        var originalObj = data.pointerDrag;
+        if (originalObj == null)
+            return null;
+
+        var dragMe = originalObj.GetComponent<DragMe>();
+        if (dragMe == null)
+            return null;
+
+        string instru = dragMe.instruction;
+        if (instru == null)
+            return null;
+
+        return instru;
+    }
+}
